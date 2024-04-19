2f0e73032d91c03107a30c2ea27706bb6e5e6660
Ellis Arn <ellisarn@gmail.com>
2024-04-19 14:36:26 +0200

    besseres dropdown

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,38 @@
-import { useState } from 'react'
+import React, { useEffect } from 'react'
 import './App.css'
-import { Topic } from './Topic.jsx'
 
 function App() {
+  useEffect(() => {
+    const subjects = document.querySelectorAll('.subjects')
+    const topics = document.querySelectorAll('.topics')
+    const toggle = document.querySelector('#toggle')
+
+    subjects.forEach((subject) => {
+      subject.addEventListener('click', (event) => {
+        if (toggle.checked) {
+          toggle.checked = false
+          toggleOptions()
+        } else {
+          toggle.checked = true
+          toggleOptions()
+        }
+        document.querySelector('#labelToToggle').innerHTML = subject.innerHTML
+      })
+    })
+
+    toggle.addEventListener('change', toggleOptions)
+
+    return () => {
+      subjects.forEach((subject) => {
+        subject.removeEventListener('click', handleClick)
+      })
+
+      topics.forEach((topic) => {
+        topic.removeEventListener('click', handleClick)
+      })
+    }
+  }, [])
+
   return (
     <>
       <div className="container">
@@ -12,11 +42,19 @@ function App() {
         </header>
         <main>main</main>
         <nav>
-          <select name="subjects" id="dropdown">
-            <option value="s1">s1</option>
-            <option value="s2">s2</option>
-            <option value="s3">s3</option>
-          </select>
+          <label id="labelToToggle" htmlFor="toggle">
+            s1
+          </label>
+          <input type="checkbox" id="toggle"></input>
+          <div className="subjects" value="s1">
+            s1
+          </div>
+          <div className="subjects" value="s2">
+            s2
+          </div>
+          <div className="subjects" value="s3">
+            s3
+          </div>
           <div className="topics" id="topic-1">
             t1
           </div>
@@ -32,25 +70,27 @@ function App() {
   )
 }
 
-document.querySelectorAll('.topics').forEach((topic) => {
-  topic.addEventListener('click', (event) => {
-    document.querySelector(
-      'nav'
-    ).innerHTML = `<select name="topics" id="dropdown">
-    <option value="t1">t1</option>
-    <option value="t2">t2</option>
-    <option value="t3">t3</option>
-  </select>
-  <div className="assignment" id="assignment-1">
-    a1
-  </div>
-  <div className="assignment" id="assignment-2">
-    a2
-  </div>
-  <div className="assignment" id="assignment-3">
-    a3
-  </div>`
-  })
-})
+function toggleOptions() {
+  console.log('toggleOptions', toggle.checked)
+  if (toggle.checked) {
+    document.querySelectorAll('.subjects').forEach((subject) => {
+      subject.style.display = 'block'
+    })
+    document.querySelectorAll('.topics').forEach((topic) => {
+      topic.style.display = 'none'
+    })
+  } else {
+    document.querySelectorAll('.subjects').forEach((subject) => {
+      subject.style.display = 'none'
+    })
+    document.querySelectorAll('.topics').forEach((topic) => {
+      topic.style.display = 'block'
+    })
+  }
+}
+
+function handleClick(event) {
+  console.log(event.target)
+}
 
 export default App
