1a2ec7fdf6ca665af9e6adba1c2ef7f88877f3de
Ellis Arn <ellisarn@gmail.com>
2024-04-20 11:19:15 +0200

    headerbuttons

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,65 @@ import './App.css'
 
 function App() {
   useEffect(() => {
+    const headerButtons = document.querySelectorAll('.headerButtons')
     const subjects = document.querySelectorAll('.subjects')
     const topics = document.querySelectorAll('.topics')
     const toggle = document.querySelector('#toggle')
 
+    let selectedHeaderButton = document.querySelector('.selected')
+
     let selectedSubject = document.querySelector('#labelToToggle').innerHTML
+    document.querySelector(
+      '#textfield'
+    ).value = `text in topic-1 in ${selectedSubject}`
+
+    function handleHeaderButtonClick(event) {
+      selectedHeaderButton.classList.remove('selected')
+      event.target.classList.add('selected')
+      selectedHeaderButton = event.target
+
+      if (event.target.id === 'start-headerButton') {
+        document.querySelector('#bottom-headerButtons').innerHTML = ''
+        for (let i = 1; i <= 22; i++) {
+          const button = document.createElement('button')
+          button.classList.add('editButtons')
+          button.innerHTML = i
+          document.querySelector('#bottom-headerButtons').appendChild(button)
+        }
+      } else if (event.target.id === 'insert-headerButton') {
+        document.querySelector('#bottom-headerButtons').innerHTML = ''
+        for (let i = 1; i <= 16; i++) {
+          const button = document.createElement('button')
+          button.classList.add('editButtons')
+          button.innerHTML = i
+          document.querySelector('#bottom-headerButtons').appendChild(button)
+        }
+      } else if (event.target.id === 'draw-headerButton') {
+        document.querySelector('#bottom-headerButtons').innerHTML = ''
+        for (let i = 1; i <= 16; i++) {
+          const button = document.createElement('button')
+          button.classList.add('editButtons')
+          button.innerHTML = i
+          document.querySelector('#bottom-headerButtons').appendChild(button)
+        }
+      } else if (event.target.id === 'view-headerButton') {
+        document.querySelector('#bottom-headerButtons').innerHTML = ''
+        for (let i = 1; i <= 15; i++) {
+          const button = document.createElement('button')
+          button.classList.add('editButtons')
+          button.innerHTML = i
+          document.querySelector('#bottom-headerButtons').appendChild(button)
+        }
+      } else if (event.target.id === 'help-headerButton') {
+        document.querySelector('#bottom-headerButtons').innerHTML = ''
+        for (let i = 1; i <= 4; i++) {
+          const button = document.createElement('button')
+          button.classList.add('editButtons')
+          button.innerHTML = i
+          document.querySelector('#bottom-headerButtons').appendChild(button)
+        }
+      }
+    }
 
     function handleSubjectClick(event) {
       selectedSubject = event.target.innerHTML
@@ -33,6 +87,10 @@ function App() {
       ).value = `text in ${event.target.id} in ${selectedSubject}`
     }
 
+    headerButtons.forEach((button) => {
+      button.addEventListener('click', handleHeaderButtonClick)
+    })
+
     subjects.forEach((subject) => {
       subject.addEventListener('click', handleSubjectClick)
     })
@@ -57,13 +115,74 @@ function App() {
     <>
       <div className="container">
         <header>
-          <div id="header-top">1</div>
-          <div id="header-bottom">2</div>
+          <div id="header-top">
+            <div id="top-headerButtons">
+              <div id="menuButton-container">
+                <button
+                  className="headerButtons menuButtons selected"
+                  id="start-headerButton"
+                >
+                  Start
+                </button>
+                <button
+                  className="headerButtons menuButtons"
+                  id="insert-headerButton"
+                >
+                  Einfügen
+                </button>
+                <button
+                  className="headerButtons menuButtons"
+                  id="draw-headerButton"
+                >
+                  Zeichnen
+                </button>
+                <button
+                  className="headerButtons menuButtons"
+                  id="view-headerButton"
+                >
+                  Ansicht
+                </button>
+                <button
+                  className="headerButtons menuButtons"
+                  id="help-headerButton"
+                >
+                  Hilfe
+                </button>
+              </div>
+              <div id="otherButton-container">
+                <button
+                  className="headerButtons otherButtons"
+                  id="profile-headerButton"
+                >
+                  Profil
+                </button>
+                <button
+                  className="headerButtons otherButtons"
+                  id="share-headerButton"
+                >
+                  Teilen
+                </button>
+                <button
+                  className="headerButtons otherButtons"
+                  id="print-headerButton"
+                >
+                  Drucken
+                </button>
+                <button
+                  className="headerButtons otherButtons"
+                  id="settings-headerButton"
+                >
+                  Einstellungen
+                </button>
+              </div>
+            </div>
+          </div>
+          <div id="header-bottom">
+            <div id="bottom-headerButtons"></div>
+          </div>
         </header>
         <main>
-          <textarea name="textfield" id="textfield">
-            text in topic-1 in s1
-          </textarea>
+          <textarea name="textfield" id="textfield"></textarea>
         </main>
         <nav>
           <label id="labelToToggle" htmlFor="toggle">
